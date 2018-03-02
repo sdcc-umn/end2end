83987bf69a121de0e4a8c6edd78090d1cc15732a
Nathan Bittner <njbittner@gmail.com>
2018-03-02 17:24:46 -0600

    gave ability to predict, even if it sucks.

--- train.py
+++ train.py
@@ -20,26 +20,26 @@ from ops.tf_ops import *
 from ops.data_ops import *
 
 
-if __name__ == '__main__':
-
+def main(pred_img=None):
    parser = argparse.ArgumentParser()
-   parser.add_argument('--BATCH_SIZE', required=False,help='Batch size',type=int,default=32)
+   parser.add_argument('--BATCH_SIZE', required=False, help='Batch size', type=int, default=32)
    # parser.add_argument('--DATA_DIR',   required=True,help='~/SDCC/data')
-   parser.add_argument('--TRIAL',      required=True,help='Trial number', type=int)
-   parser.add_argument('--NETWORK',    required=False,help='The network to use', default='simple_cnn')
-   parser.add_argument('--EPOCHS',     required=False,help='How long to train',type=int,default=5)
-   parser.add_argument('--STACK',      required=False,help='Number of frames to stack',type=int,default=4)
+   # parser.add_argument('--TRIAL', required=True, help='Trial number', type=int)
+   parser.add_argument('--NETWORK', required=False, help='The network to use', default='simple_cnn')
+   parser.add_argument('--EPOCHS', required=False, help='How long to train', type=int, default=5)
+   parser.add_argument('--STACK', required=False, help='Number of frames to stack', type=int, default=4)
    a = parser.parse_args()
-
    BATCH_SIZE = a.BATCH_SIZE
    # DATA_DIR   = a.DATA_DIR
    NETWORK    = a.NETWORK
    EPOCHS     = a.EPOCHS
    #STACK      = a.STACK
    STACK = 1
-   TRIAL      = a.TRIAL
+   #TRIAL      = a.TRIAL
+   TRIAL = 2
 
    CHECKPOINT_DIR = 'checkpoints/NETWORK_'+NETWORK+'/STACK_'+str(STACK)+'/TRIAL_'+str(TRIAL)
+   # CHECKPOINT_DIR = 'checkpoints/NETWORK_'+NETWORK+'/STACK_'+str(STACK)
    
    try: os.makedirs(CHECKPOINT_DIR)
    except: pass
@@ -80,7 +80,10 @@ if __name__ == '__main__':
 
    # restore previous model if there is one
    ckpt = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
+   print(CHECKPOINT_DIR)
    if ckpt and ckpt.model_checkpoint_path:
+      print("loading model")
+
       LOGGER.info("Restoring previous model...")
       try:
          saver.restore(sess, ckpt.model_checkpoint_path)
@@ -89,34 +92,45 @@ if __name__ == '__main__':
          LOGGER.info("Could not restore model")
          pass
 
-   ## Begin training
-   step = sess.run(global_step)
-   batch = 1
-   b = batchGenerator("trial_2", n_stack=STACK, inf=False)
+   if pred_img is None:
+       ## Begin training
+       step = sess.run(global_step)
+       batch = 1
+       b = batchGenerator("trial_2", n_stack=STACK, inf=False)
 
-   for epoch_c in range(EPOCHS):
-      for res in b:
-          if res == -1:
-              break
-          batch_images, batch_control = res
-          # run train op
-          sess.run(train_op, feed_dict={images:batch_images, control:batch_control})
+       for epoch_c in range(EPOCHS):
+          for res in b:
+              if res == -1:
+                  break
+              batch_images, batch_control = res
+              # run train op
+              sess.run(train_op, feed_dict={images:batch_images, control:batch_control})
 
-          # now get all losses and summary *without* performing a training step - for tensorboard
-          loss_batch, summary = sess.run([loss, merged_summary_op], feed_dict={images:batch_images, control:batch_control})
-          summary_writer.add_summary(summary, step)
+              # now get all losses and summary *without* performing a training step - for tensorboard
+              loss_batch, summary = sess.run([loss, merged_summary_op], feed_dict={images:batch_images, control:batch_control})
+              summary_writer.add_summary(summary, step)
 
-          print('epoch:',epoch_c,'step:',step,'batch_loss:',loss_batch)
-          step += 1
+              print('epoch:',epoch_c,'step:',step,'batch_loss:',loss_batch)
+              step += 1
 
-          if step%500 == 0:
-             LOGGER.info('Saving model...')
-             saver.save(sess, CHECKPOINT_DIR+'checkpoint-'+str(step))
-             saver.export_meta_graph(CHECKPOINT_DIR+'checkpoint-'+str(step)+'.meta')
-             LOGGER.info('Done saving')
+              if step%500 == 0:
+                 LOGGER.info('Saving model...')
+                 saver.save(sess, CHECKPOINT_DIR+'checkpoint-'+str(step))
+                 saver.export_meta_graph(CHECKPOINT_DIR+'checkpoint-'+str(step)+'.meta')
+                 LOGGER.info('Done saving')
 
-          batch+=1
+              batch+=1
+       saver.save(sess, CHECKPOINT_DIR+'checkpoint-'+str(step))
+       saver.export_meta_graph(CHECKPOINT_DIR + 'checkpoint-' + str(step) + '.meta')
 
-   saver.save(sess, CHECKPOINT_DIR+'checkpoint-'+str(step))
-   saver.export_meta_graph(CHECKPOINT_DIR + 'checkpoint-' + str(step) + '.meta')
+   else:
+       img = np.reshape(get_image(pred_img), (1, 96, 128, 1))
+       full_img = img
+       for i in range(31):
+           img = np.concatenate((full_img, img), axis=0)
+       out = sess.run(pred, {images:img})
+       print(out)
    sess.close()
+
+if __name__ == "__main__":
+    main()
